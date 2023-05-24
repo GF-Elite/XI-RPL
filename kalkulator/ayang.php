3129c8f6680a7fdd68ab5408620605bdb2098b62
GF-Elite <litageming@gmail.com>
2023-05-24 22:15:50 +0700

    first commit

---
+++ kalkulator/ayang.php
@@ -0,0 +1,19 @@
+<?php
+ 
+$pesan = [
+    "Kamu adalah API yang selalu memberikan kebahagiaan dalam hidupku.",
+    "Ketika aku melihat kamu, database hatiku langsung ter-update dengan cinta.",
+    "Aku ingin menjadi fungsi dalam hidupmu, agar aku bisa selalu menghasilkan kebahagiaan untukmu.",
+    "Kamu seperti tag <b>strong</b> dalam HTML, kamu membuat hidupku menjadi lebih kuat dan berarti.",
+    "Kamu seperti URL yang selalu mengarahkan aku kepada kebahagiaan.",
+    "Setiap kali aku melihat kamu, aku merasa seperti melakukan echo 'I love you' dalam hatiku.",
+];
+
+
+$kata = $pesan[array_rand($pesan)];
+
+
+echo "<h1>ayang tria</h1>";
+echo "<p>{$kata}</p>";
+
+?>
